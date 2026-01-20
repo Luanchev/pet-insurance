a794bf5431b7bfe8ec4221618aefa5598b1ae2f7
luanchev <luanchev3@gmail.com>
2026-01-19 19:44:28 -0500

    v1 - init

---
+++ src/main/java/com/co/ias/aseguratupata/pet_insurance/PetInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.co.ias.aseguratupata.pet_insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/ias/aseguratupata/pet_insurance/PetInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.ias.aseguratupata.pet_insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
